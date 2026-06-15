d8ab1d715198dbc95399015540e82777f2d587c9
Sam Hadfield <sthtech@protonmail.com>
2026-06-15 14:47:52 -0500

    feat: add route/controller fn to render homepage

--- app.js
+++ app.js
@@ -2,11 +2,14 @@ const express = require("express");
 const app = express();
 const path = require("node:path");
 const { body, validationResult, matchedData } = require("express-validator");
+const indexRouter = require("./routes/index");
 
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+
+app.use("/", indexRouter);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
