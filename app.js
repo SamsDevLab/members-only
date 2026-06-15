a197105e38fc77833950a549abe941262d8a537d
Sam Hadfield <sthtech@protonmail.com>
2026-06-15 14:33:01 -0500

    build: setup app file – ensure port is listening

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const path = require("node:path");
+const { body, validationResult, matchedData } = require("express-validator");
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}!`);
+});
