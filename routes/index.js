1eb8de15a05c7e6e9a9108435ccacff1b51b06f3
Sam Hadfield <sthtech@protonmail.com>
2026-06-16 11:00:35 -0500

    refactor: use to get request

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,6 @@ const express = require("express");
 const router = express.Router();
 const indexController = require("../controllers/index");
 
-router.use("/", indexController.getHomepage);
+router.get("/", indexController.getHomepage);
 
 module.exports = router;
