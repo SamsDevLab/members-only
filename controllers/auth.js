fc0b46a5537df21e0996bdf193af8afb66b16cf3
Sam Hadfield <sthtech@protonmail.com>
2026-06-24 10:34:11 -0500

    refactor: re-add logOut to controller
    Re-add logOut function to controller as actual logout function should be
    placed here rather than in the route itself

--- controllers/auth.js
+++ controllers/auth.js
@@ -16,10 +16,20 @@ function submitNewUser(req, res) {
   usersModel.addNewUser(newUser);
 }
 
+function logOut(req, res, next) {
+  req.logout(function (err) {
+    if (err) {
+      return next(err);
+    }
+    res.redirect("/");
+  });
+}
+
 // This controller will also include login, becoming a member, becoming admin as these all fall under auth
 
 module.exports = {
   renderLoginForm,
   renderSignUpForm,
   submitNewUser,
+  logOut,
 };
--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,20 @@
 const express = require("express");
 const router = express.Router();
+const passport = require("passport");
 const authController = require("../controllers/auth.js");
 const validateUserSignUp = require("../middleware/validators.js");
 
 router.get("/log-in", authController.renderLoginForm);
-router.post("/log-in", authController.logInUser);
+router.post(
+  "/log-in",
+  passport.authenticate("local", (error, user, info) => {
+    if (error) {
+      console.log(error);
+    }
+  }),
+);
+
+router.get("/log-out", authController.logOut);
 
 router.get("/sign-up", authController.renderSignUpForm);
 router.post("/sign-up", validateUserSignUp, authController.submitNewUser);
