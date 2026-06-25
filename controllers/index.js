9fcc9045f4c48570dd680b618a6a9426258c8091
Sam Hadfield <sthtech@protonmail.com>
2026-06-25 13:44:15 -0500

    feat: query all messages, render in index view
    Query all messages from database when get request is made to home route.
    Add all available messages to message board in index view

--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,11 @@
+const messagesModel = require("../models/messages");
+
 async function getHomepage(req, res) {
-  res.render("index", { title: "Boat Club Messaging App" });
+  const allMessages = await messagesModel.getAllMessages();
+  res.render("index", {
+    title: "Boat Club Messaging App",
+    messages: allMessages,
+  });
 }
 
 module.exports = { getHomepage };
