4144d1ce00095cd5a0666ed5dc18c952e1d30d90
Sam Hadfield <sthtech@protonmail.com>
2026-06-26 09:06:23 -0500

    fix: extract err.errors rather than err.messages

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,12 +1,12 @@
 function errorHandler(err, req, res, next) {
   const statusCode = err.statusCode || 500;
-  const msgArr = err.errors.map((error) => error.msg);
+  const errorMessages = err.errors;
 
   res.status(statusCode).json({
     success: false,
     error: {
       status: statusCode,
-      messages: msgArr,
+      messages: errorMessages,
     },
   });
 }
