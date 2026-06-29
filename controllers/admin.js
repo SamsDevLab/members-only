a62560d2433bf7248f4b23329ea5fa9d226deace
Sam Hadfield <sthtech@protonmail.com>
2026-06-29 09:01:13 -0500

    feat: add Admin Sign Up view with password check
    Add Admin Signup view and move admin GET request to render the Admin
    signup view. Add a secret admin password and create password check when
    member attempts to become an admin

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,5 +1,30 @@
-async function attemptUpgradeToAdmin() {
-  console.log("testing");
+const bcrypt = require("bcryptjs");
+
+function renderAdminSignup(req, res) {
+  const user = req.user;
+  res.render("auth/admin", {
+    title: "Admin Sign Up",
+    currentUser: user,
+    errMsg: null,
+  });
 }
 
-module.exports = { attemptUpgradeToAdmin };
+async function attemptUpgradeToAdmin(req, res) {
+  const passwordAttempt = req.body.passwordAttempt;
+  const match = await bcrypt.compare(
+    passwordAttempt,
+    process.env.ADMIN_PASSWORD,
+  );
+
+  if (!match) {
+    res.render("auth/admin", {
+      title: "Admin Sign Up",
+      currentUser: req.user,
+      errMsg: "Incorrect password",
+    });
+  } else {
+    console.log("Justin Townes Earle");
+  }
+}
+
+module.exports = { attemptUpgradeToAdmin, renderAdminSignup };
--- routes/admin.js
+++ routes/admin.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const adminController = require("../controllers/admin");
 
-router.get("/", adminController.attemptUpgradeToAdmin);
+router.get("/", adminController.renderAdminSignup);
+router.post("/", adminController.attemptUpgradeToAdmin);
 
 module.exports = router;
